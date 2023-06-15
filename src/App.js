86cb344c96d41ec2e00c7c01899440bc137a003a
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-06-15 23:33:55 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -18,21 +18,21 @@ function App() {
 		<div className="App">
 			{authIsReady && (
 				<BrowserRouter>
-				<Routes>
-					<Route path="*" element={
-					<div className="container">
-						<Navbar />
-						<Routes>
-							<Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-							<Route path="/create" element={user ? <Create /> : <Navigate to="/login" />} />
-							<Route path="/projects/:id" element={user ? <Project /> : <Navigate to="/login" />} />
-							<Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-							<Route path="/signup" element={user && user.displayName ? <Navigate to="/" /> : <Signup />} />
-							<Route path="*" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-						</Routes>
-					</div>} />
-				</Routes>
-				x
+					{ user && <Sidebar /> }
+					<Routes>
+						<Route path="*" element={
+						<div className="container">
+							<Navbar />
+							<Routes>
+								<Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+								<Route path="/create" element={user ? <Create /> : <Navigate to="/login" />} />
+								<Route path="/projects/:id" element={user ? <Project /> : <Navigate to="/login" />} />
+								<Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+								<Route path="/signup" element={user && user.displayName ? <Navigate to="/" /> : <Signup />} />
+								<Route path="*" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+							</Routes>
+						</div>} />
+					</Routes>
 				</BrowserRouter>
 			)}
 		</div>
