63222f4257884fd45bd01372d6ba6e51dca0d35d
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-06-16 23:31:28 +0530

    Project List component added

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,17 @@
+import useCollection from '../../hooks/useCollection'
+import ProjectList from './ProjectList'
+
 //styles 
 import './Dashboard.css'
 
 export default function Dashboard(){
+    const { documents, isPending, error } = useCollection('projects')
     
     return (
         <div>
-            Dashboard
+            <h2 className="page-title">Dashboard</h2>
+            {error && <p className="error">{error}</p>}
+            {documents && <ProjectList projects={documents} />}
         </div>
     )
 }
